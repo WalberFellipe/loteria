c251ca8a275a9abde3621622fba0c5a4b597b4ee
Walber Fellipe <walberfellipe18@gmail.com>
2021-02-06 11:36:49 -0300

    User entity and resources

---
+++ src/main/java/com/walber/loteria/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.walber.loteria.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.walber.loteria.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	/*endpoint para acesso de usuários */
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "maria", "walberfellipe@gmail.com");
+		return ResponseEntity.ok().body(u);
+	}
+}
